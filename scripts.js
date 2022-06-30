967fb0460ee67f80ae7f28b128927df2bc6ab229
Henna Paananen <83033122+henzki@users.noreply.github.com>
2022-06-30 10:44:47 +0200

    Final commit

---
+++ scripts.js
@@ -0,0 +1,29 @@
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+
+var goUp = document.getElementById("goUp");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        goUp.style.display = "block";
+    } else {
+        goUp.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
